6d7254037ec7027c9e1b0720fcc52a86898d578c
bertolinocastro <jgabbc@hotmail.com>
2020-04-20 01:33:30 -0300

    Extending integration time do stipulation

--- adjust_temporal_serie_4_br_cities.py
+++ adjust_temporal_serie_4_br_cities.py
@@ -81,14 +81,14 @@ def cost_function(params, observed, meta, t, predef_param):
     return np.r_[err_I, err_D]
 
 
-def stipulation(thr, observed, meta):
+def stipulation(thr, extra_days, observed, meta):
 
     N = meta[0] # unpacking state metadata
     t_lth = observed[4] # getting time series length
 
     # thrs -> ini + 5, fin - 5
     m = (5, 5) # margins after beginning and before today for intervention fitting
-    if t_lth-sum(m) < 0:
+    if t_lth-sum(m) <= 0:
         m = (m[0]+(t_lth-sum(m)-.01) , m[1]) # shifting lower bound to fit inside time interval. upper bound remain intact
 
     boundaries = np.array([
@@ -120,8 +120,8 @@ def stipulation(thr, observed, meta):
     best_cost   = np.inf  # holding minimized cost set of parameters
     best_params = (None,) # tuple holding set of best parameters
 
-    # for tries in range(40):
-    for tries in range(5):
+    # for tries in range(25):
+    for tries in range(1):
 
         params0 = np.random.rand(boundaries.shape[1])
         params0 = boundaries[0] + params0*(boundaries[1]-boundaries[0])
@@ -134,6 +134,7 @@ def stipulation(thr, observed, meta):
             # print(f'[{thr}]:{meta[2]}/{meta[3]} Found cost {best_cost} with params:\n{best_params}\n')
 
 
+    t = np.arange(0,1+extra_days+t_lth) # timespan based on days length and a week ahead
     # getting containers
     containers = get_containers(best_params, predef_param, observed, meta, t)
 
@@ -153,8 +154,8 @@ def observed_data():
     for id in ids:
         city_lines = raw[raw.ibgeID == id]
 
-        # skipping time series with less than 7 points
-        if city_lines.shape[0] < 7: continue
+        # skipping time series with less than 10 points
+        if city_lines.shape[0] < 10: continue
 
         cities[id] = (
             city_lines.date.values,       # [0] -> date       vector
@@ -183,15 +184,24 @@ def city_metadata():
 
     return metadata
 
+def save_time_series(params, predef_param, containers, meta, extra_days):
 
-def plot_compare(params, predef_param, containers, observed, meta):
+    _, ibgeID, city, state = meta
+
+    beta1, beta2, delta, ha, ksi, t_thresh = params[:-3]
+    kappa, p, gamma_asym, gamma_sym, gamma_H, gamma_U, mi_H, mi_U, omega = predef_param
+
+
+    outpath = f'{out_folder()}/{state}/{city}/time_series_extraDays={extra_days}.csv'
+
+    np.savetxt(outpath, containers, delimiter=',', newline='\n', header='S,I_A,I_S,E,H,U,R,D,Nw', fmt=['%d','%d','%d','%d','%d','%d','%d','%d','%d'])
+
+def plot_compare(params, predef_param, containers, observed, meta, extra_days):
 
     N, _, city, state = meta # unpacking state metadata
 
-    t = np.arange(1,1+len(observed[0])) # timespan based on days length
     y = containers[1:]
-
-    # np.savetxt('TESTE.csv', containers, delimiter=',', newline='\n', header='S,I_A,I_S,E,H,U,R,D,Nw', fmt=['%d','%d','%d','%d','%d','%d','%d','%d','%d'])
+    t = np.arange(1,1+y.shape[0]) # timespan based on days length
 
     deaths = np.nonzero(observed[1])[0]
     ini = deaths[0] if len(deaths) else -1
@@ -200,13 +210,16 @@ def plot_compare(params, predef_param, containers, observed, meta):
     # plot step
     fig = plt.figure(0)
 
+    vl = plt.axvline(params[5], c='r', ls='--', label='Start of Intervention')
+    vL = plt.axvline(t.shape[0]-extra_days, c='#6d4848', ls=':', label='Time Series Limit')
+
     # plotting Deaths
     if ini > 0:
-        od, = plt.plot(t[ini:], observed[1][ini:], 'o', mfc='none', mec='#f47f18', label='Observed Death', ms=4, mew=1.)
+        od, = plt.plot(t[ini:-extra_days], observed[1][ini:], 'o', mfc='none', mec='#f47f18', label='Observed Death', ms=4, mew=1.)
         sd, = plt.plot(t[ini:], y[ini:,-2], '-r', label='Stipulated Death', lw=1)
 
     si, = plt.plot(t, y[:,-1], '-b', label='Stipulated Infection', lw=1)
-    oi, = plt.plot(t, observed[3], '.k', label='Observed Infection', ms=5)
+    oi, = plt.plot(t[:-extra_days], observed[3], '.k', label='Observed Infection', ms=5)
 
 
     # r, = plt.plot(t, y[:,-3], '-g', label='Stipulated Recovery', lw=1)
@@ -217,7 +230,6 @@ def plot_compare(params, predef_param, containers, observed, meta):
     # a, = plt.plot(t, y[:,-8], '-y', label='Stipulated Asymptomatic', lw=1)
     # s, = plt.plot(t, y[:,-9], '-', c='#f925eb', label='Stipulated Susceptible', lw=1)
 
-    vl = plt.axvline(params[5], c='r', ls='--', label='Start of Intervention')
 
     plt.legend(loc='upper left', fontsize=5)
 
@@ -227,7 +239,15 @@ def plot_compare(params, predef_param, containers, observed, meta):
     plt.title(f'Covid-19 - {city}/{state}')
 
     l = np.int32(np.floor(np.linspace(0, dates.size-1, 10)))
-    plt.xticks(t[l], dates[l], rotation=45, rotation_mode='anchor', ha='right')
+    L = np.int32(np.ceil(np.linspace(0, extra_days, 3)[1:]))
+    ex_dates = [str(np.datetime64(dates[-1])+np.timedelta64(x,'D')) for x in L]
+
+    ticks = np.r_[t[l],t[L-extra_days-1]]
+    lbls  = np.r_[dates[l],ex_dates]
+    tks = plt.xticks(ticks, lbls, rotation=45, rotation_mode='anchor', ha='right')
+    tks[1][-1].set_color('r') # setting stipulated days as red
+    tks[1][-2].set_color('r') # setting stipulated days as red
+
     plt.tight_layout()
 
     outpath = f'{out_folder()}/{state}/{city}'
@@ -242,6 +262,7 @@ def plot_compare(params, predef_param, containers, observed, meta):
     si.set_label('Infecções Estipuladas')
     oi.set_label('Infecções Reportadas')
     vl.set_label('Início da Intervenção')
+    vL.set_label('Fim da Série Temporal')
     plt.legend(loc='upper left', fontsize=10)
     plt.xlabel('Data', fontsize=17)
     plt.ylabel('Casos Reportados', fontsize=17)
@@ -290,6 +311,7 @@ def out_folder():
 
 
 def hard_work(q, thr, cities, city_meta, observed):
+    extra_days = 7
 
     c = 0; tot = len(cities)
     # stipulation per state
@@ -297,12 +319,14 @@ def hard_work(q, thr, cities, city_meta, observed):
         c+=1
         print(f'{thr}:[{c}/{tot}] Started {city}, {city_meta[city][2]}/{city_meta[city][3]}...\n')
 
-        params, predef_param, best_cost, containers = stipulation(thr, observed[city], city_meta[city])
+        params, predef_param, best_cost, containers = stipulation(thr, extra_days, observed[city], city_meta[city])
 
-        plot_compare(params, predef_param, containers, observed[city], city_meta[city])
+        plot_compare(params, predef_param, containers, observed[city], city_meta[city], extra_days)
+
+        save_time_series(params, predef_param, containers, city_meta[city], extra_days)
 
         # adding numpy row to queue
-        q.put(return_put(params, predef_param, best_cost, containers[-1], city_meta[city], observed[city][4]))
+        q.put(return_put(params, predef_param, best_cost, containers[-1-extra_days], city_meta[city], observed[city][4]))
 
 
 def main():
@@ -314,12 +338,13 @@ def main():
     # TODO: verify if there are unmatching state keys between previous dictionaries
 
     cities = np.array(list(set(observed.keys()) & set(city_meta.keys())))
-    cities = cities[:11]
+    # cities = cities[:11]
     cities = np.array([2927408])
+    # cities = np.array([4305454])
     tot = cities.shape[0]
     print(f'{tot} cities to be processed...\n')
 
-    n_thread = 4
+    n_thread = 1
     l = tot//n_thread
     r = tot%n_thread
 
